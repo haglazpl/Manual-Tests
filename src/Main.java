55302d086a23d4043ca40a2bb45152c605c64e61
haglazpl <mcziter@gmail.com>
2023-11-05 22:14:41 +0100

    Code delivery for anagram app

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    static String text1 = "Silent";
+    static String text2 = "Listen";
+
+    public static void main(String[] args) {
+        AnagramVerificationApp app = new AnagramVerificationApp();
+        app.run(text1, text2);
+    }
+}
